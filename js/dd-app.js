5f19bf110f3feae59dedb020d41eec500d210374
Carlo s A. Guillen <caguillen@google.com>
2014-06-13 13:46:00 -0700

    refactor(dd-app): add all functions and globals to one global object, ddApp

--- js/dd-app.js
+++ js/dd-app.js
@@ -1,168 +1,170 @@
-var windowElements = Array.prototype.slice.call(document.getElementsByTagName("*"));
-var DEFAULT = true, CUSTOM = false;
-var defaultDirectives = {
-  'abbr' : 'abbr',
-  'accept': 'accept',
-  'accesskey': 'accesskey',
-  'action': 'action',
-  'align': 'align',
-  'alt':'alt',
-  'background':'background',
-  'bgcolor': 'bgcolor',
-  'border': 'border',
-  'cellpadding': 'cellpadding',
-  'char': 'char',
-  'charoff': 'charoff',
-  'charset': 'charset',
-  'checked': 'checked',
-  'cite': 'cite',
-  'class': 'class',
-  'classid': 'classid',
-  'code': 'code',
-  'codebase': 'codebase',
-  'color': 'color',
-  'cols': 'cols',
-  'colspan': 'colspan',
-  'content': 'content',
-  'data': 'data',
-  'defer': 'defer',
-  'dir': 'dir',
-  'face': 'face',
-  'for': 'for',
-  'frame': 'frame',
-  'frameborder': 'frameborder',
-  'headers': 'headers',
-  'height': 'height',
-  'href': 'href',
-  'id': 'id',
-  'label': 'label',
-  'lang': 'lang',
-  'language': 'language',
-  'link': 'link',
-  'marginheight': 'marginheight',
-  'marginwidth': 'marginwidth',
-  'maxlength':'maxlength',
-  'media': 'media',
-  'multiple': 'multiple',
-  'name': 'name',
-  'object': 'object',
-  'onblur': 'onblur',
-  'onchange': 'onchange',
-  'onclick': 'onclick',
-  'onfocus': 'onfocus',
-  'onkeydown':'onkeydown',
-  'onkeypress': 'onkeypress',
-  'onkeyup': 'onkeyup',
-  'onload': 'onload',
-  'onmousedown': 'onmousedown',
-  'onmousemove': 'onmousemove',
-  'onmouseout': 'onmouseout',
-  'onmouseover': 'onmouseover',
-  'onmouseup': 'onmouseup',
-  'onreset': 'onreset',
-  'onselect': 'onselect',
-  'onsubmit': 'onsubmit',
-  'readonly': 'readonly',
-  'rel': 'rel',
-  'rev': 'rev',
-  'rows': 'rows',
-  'rowspan': 'rowspan',
-  'size': 'size',
-  'span': 'span',
-  'src': 'src',
-  'start': 'start',
-  'style': 'style',
-  'text': 'text',
-  'target': 'target',
-  'title': 'title',
-  'type': 'type',
-  'value': 'value',
-  'width': 'width',
-  'ng-app': 'ng-app',
-  'ng-bind': 'ng-bind',
-  'ng-bindhtml': 'ng-bindhtml',
-  'ng-bindtemplate': 'ng-bindtemplate',
-  'ng-blur': 'ng-blur',
-  'ng-change': 'ng-change',
-  'ng-checked': 'ng-checked',
-  'ng-class': 'ng-class',
-  'ng-classeven': 'ng-classeven',
-  'ng-classodd': 'ng-classodd',
-  'ng-click': 'ng-click',
-  'ng-cloak': 'ng-cloak',
-  'ng-controller': 'ng-controller',
-  'ng-copy': 'ng-copy',
-  'ng-csp': 'ng-csp',
-  'ng-cut': 'ng-cut',
-  'ng-dblclick': 'ng-dblclick',
-  'ng-disabled': 'ng-disabled',
-  'ng-focus': 'ng-focus',
-  'ng-form': 'ng-form',
-  'ng-hide': 'ng-hide',
-  'ng-href': 'ng-href',
-  'ng-if': 'ng-if',
-  'ng-include': 'ng-include',
-  'ng-init': 'ng-init',
-  'ng-keydown': 'ng-keydown',
-  'ng-keypress': 'ng-keypress',
-  'ng-keyup': 'ng-keyup',
-  'ng-list': 'ng-list',
-  'ng-model': 'ng-model',
-  'ng-modeloptions': 'ng-modeloptions',
-  'ng-mousedown': 'ng-mousedown',
-  'ng-mouseenter': 'ng-mouseenter',
-  'ng-mouseleave': 'ng-mouseleave',
-  'ng-mousemove': 'ng-mousemove',
-  'ng-mouseover': 'ng-mouseover',
-  'ng-mouseup': 'ng-mouseup',
-  'ng-nonbindable': 'ng-nonbindable',
-  'ng-open': 'ng-open',
-  'ng-paste': 'ng-paste',
-  'ng-pluralize': 'ng-pluralize',
-  'ng-readonly': 'ng-readonly',
-  'ng-repeat': 'ng-repeat',
-  'ng-selected': 'ng-selected',
-  'ng-show': 'ng-show',
-  'ng-src': 'ng-src',
-  'ng-srcset': 'ng-srcset',
-  'ng-style': 'ng-style',
-  'ng-submit': 'ng-submit',
-  'ng-switch': 'ng-switch',
-  'ng-transclude': 'ng-transclude',
-  'ng-value': 'ng-value'};
-var customDirectives = {'ha-breadcrumbs':'ha-breadcrumbs'}
-var failedElements = [];
-var correctionsFound = [];
-
-var checkDefaultDirs = function() {
-  windowElements.forEach(function(element) {
+var ddApp = {
+  windowElements : Array.prototype.slice.call(document.getElementsByTagName("*")),
+  DEFAULT : true,
+  CUSTOM : false,
+  defaultDirectives : {
+    'abbr' : 'abbr',
+    'accept': 'accept',
+    'accesskey': 'accesskey',
+    'action': 'action',
+    'align': 'align',
+    'alt':'alt',
+    'background':'background',
+    'bgcolor': 'bgcolor',
+    'border': 'border',
+    'cellpadding': 'cellpadding',
+    'char': 'char',
+    'charoff': 'charoff',
+    'charset': 'charset',
+    'checked': 'checked',
+    'cite': 'cite',
+    'class': 'class',
+    'classid': 'classid',
+    'code': 'code',
+    'codebase': 'codebase',
+    'color': 'color',
+    'cols': 'cols',
+    'colspan': 'colspan',
+    'content': 'content',
+    'data': 'data',
+    'defer': 'defer',
+    'dir': 'dir',
+    'face': 'face',
+    'for': 'for',
+    'frame': 'frame',
+    'frameborder': 'frameborder',
+    'headers': 'headers',
+    'height': 'height',
+    'href': 'href',
+    'id': 'id',
+    'label': 'label',
+    'lang': 'lang',
+    'language': 'language',
+    'link': 'link',
+    'marginheight': 'marginheight',
+    'marginwidth': 'marginwidth',
+    'maxlength':'maxlength',
+    'media': 'media',
+    'multiple': 'multiple',
+    'name': 'name',
+    'object': 'object',
+    'onblur': 'onblur',
+    'onchange': 'onchange',
+    'onclick': 'onclick',
+    'onfocus': 'onfocus',
+    'onkeydown':'onkeydown',
+    'onkeypress': 'onkeypress',
+    'onkeyup': 'onkeyup',
+    'onload': 'onload',
+    'onmousedown': 'onmousedown',
+    'onmousemove': 'onmousemove',
+    'onmouseout': 'onmouseout',
+    'onmouseover': 'onmouseover',
+    'onmouseup': 'onmouseup',
+    'onreset': 'onreset',
+    'onselect': 'onselect',
+    'onsubmit': 'onsubmit',
+    'readonly': 'readonly',
+    'rel': 'rel',
+    'rev': 'rev',
+    'rows': 'rows',
+    'rowspan': 'rowspan',
+    'size': 'size',
+    'span': 'span',
+    'src': 'src',
+    'start': 'start',
+    'style': 'style',
+    'text': 'text',
+    'target': 'target',
+    'title': 'title',
+    'type': 'type',
+    'value': 'value',
+    'width': 'width',
+    'ng-app': 'ng-app',
+    'ng-bind': 'ng-bind',
+    'ng-bindhtml': 'ng-bindhtml',
+    'ng-bindtemplate': 'ng-bindtemplate',
+    'ng-blur': 'ng-blur',
+    'ng-change': 'ng-change',
+    'ng-checked': 'ng-checked',
+    'ng-class': 'ng-class',
+    'ng-classeven': 'ng-classeven',
+    'ng-classodd': 'ng-classodd',
+    'ng-click': 'ng-click',
+    'ng-cloak': 'ng-cloak',
+    'ng-controller': 'ng-controller',
+    'ng-copy': 'ng-copy',
+    'ng-csp': 'ng-csp',
+    'ng-cut': 'ng-cut',
+    'ng-dblclick': 'ng-dblclick',
+    'ng-disabled': 'ng-disabled',
+    'ng-focus': 'ng-focus',
+    'ng-form': 'ng-form',
+    'ng-hide': 'ng-hide',
+    'ng-href': 'ng-href',
+    'ng-if': 'ng-if',
+    'ng-include': 'ng-include',
+    'ng-init': 'ng-init',
+    'ng-keydown': 'ng-keydown',
+    'ng-keypress': 'ng-keypress',
+    'ng-keyup': 'ng-keyup',
+    'ng-list': 'ng-list',
+    'ng-model': 'ng-model',
+    'ng-modeloptions': 'ng-modeloptions',
+    'ng-mousedown': 'ng-mousedown',
+    'ng-mouseenter': 'ng-mouseenter',
+    'ng-mouseleave': 'ng-mouseleave',
+    'ng-mousemove': 'ng-mousemove',
+    'ng-mouseover': 'ng-mouseover',
+    'ng-mouseup': 'ng-mouseup',
+    'ng-nonbindable': 'ng-nonbindable',
+    'ng-open': 'ng-open',
+    'ng-paste': 'ng-paste',
+    'ng-pluralize': 'ng-pluralize',
+    'ng-readonly': 'ng-readonly',
+    'ng-repeat': 'ng-repeat',
+    'ng-selected': 'ng-selected',
+    'ng-show': 'ng-show',
+    'ng-src': 'ng-src',
+    'ng-srcset': 'ng-srcset',
+    'ng-style': 'ng-style',
+    'ng-submit': 'ng-submit',
+    'ng-switch': 'ng-switch',
+    'ng-transclude': 'ng-transclude',
+    'ng-value': 'ng-value'},
+  customDirectives : {'ha-breadcrumbs':'ha-breadcrumbs','ha-footer':'ha-footer'},
+  failedElements : [],
+  correctionsFound : []
+}
+ddApp.checkDefaultDirs = function() {
+  ddApp.windowElements.forEach(function(element) {
     if(element.attributes.length) {
-      failedAttributes = getFailedAttributes(element.attributes);
+      failedAttributes = ddApp.getFailedAttributes(element.attributes);
       if(failedAttributes.length) {
         failedElement = {
           element: element,
           failedAttributes: failedAttributes
         };
-        failedElements.push(failedElement);
+        ddApp.failedElements.push(failedElement);
       }
     }
   });
 }
-var getSuggestions = function() {
-  failedElements.forEach(function(failed) {
-    var cDirMatch = findClosestMatchIn(CUSTOM, failed);
-    var dDirMatch = findClosestMatchIn(DEFAULT, failed);
+ddApp.getSuggestions = function() {
+  ddApp.failedElements.forEach(function(failed) {
+    var cDirMatch = ddApp.findClosestMatchIn(ddApp.CUSTOM, failed);
+    var dDirMatch = ddApp.findClosestMatchIn(ddApp.DEFAULT, failed);
     var match;
     for(var i in cDirMatch) {
       var minDifference = Math.min(cDirMatch[i].levDist,dDirMatch[i].levDist);
       var match = (minDifference == cDirMatch[i].levDist)? cDirMatch: dDirMatch;
     }
     var toPush = {domElement:failed, results: match};
-    correctionsFound.push(toPush);
+    ddApp.correctionsFound.push(toPush);
   })
 }
-var displayResults = function() {
-  correctionsFound.forEach(function(obj) {
+ddApp.displayResults = function() {
+  ddApp.correctionsFound.forEach(function(obj) {
     obj.results.forEach(function(attr) {
       id = (obj.domElement.element.id) ? 'with id: #'+obj.domElement.element.id : '';
       type = obj.domElement.element.nodeName;
@@ -172,20 +174,20 @@ var displayResults = function() {
     })
   })
 }
-var getFailedAttributes = function(attributes) {
+ddApp.getFailedAttributes = function(attributes) {
   var failedAttributes = [];
   for(var i = 0; i < attributes.length; i++) {
     var attr = attributes[i].nodeName;
-    var inDefault = attrOfEleExsistIn(DEFAULT, attr);
-    var inCustom = attrOfEleExsistIn(CUSTOM, attr);
+    var inDefault = ddApp.attrOfEleExsistIn(ddApp.DEFAULT, attr);
+    var inCustom = ddApp.attrOfEleExsistIn(ddApp.CUSTOM, attr);
     if(!inDefault && !inCustom) {
       failedAttributes.push(attr);
     }
   }
   return failedAttributes;
 }
-var findClosestMatchIn = function(isDefault, failedEle) {
-  var directiveData = (isDefault)? defaultDirectives : customDirectives;
+ddApp.findClosestMatchIn = function(isDefault, failedEle) {
+  var directiveData = (isDefault)? ddApp.defaultDirectives : ddApp.customDirectives;
   var failedAttrs = failedEle.failedAttributes;
   var correctionsToSend = [];
   failedAttrs.forEach(function(failedAttr) {
@@ -193,7 +195,7 @@ var findClosestMatchIn = function(isDefault, failedEle) {
     for(var i in directiveData){
       var attr = directiveData[i]
       if(Math.abs(failedAttr.length-attr.length) < 3) {
-        var currentlevDist = levenshteinDistance(failedAttr, attr);
+        var currentlevDist = ddApp.levenshteinDistance(failedAttr, attr);
         var closestMatch = (min_levDist > currentlevDist)? attr : closestMatch;
         var min_levDist = (min_levDist > currentlevDist)? currentlevDist : min_levDist;
       }
@@ -206,15 +208,15 @@ var findClosestMatchIn = function(isDefault, failedEle) {
   });
   return correctionsToSend;
 }
-var attrOfEleExsistIn = function(isDefault,attribute){
-  var currentAttr = (isDefault)? defaultDirectives[attribute]:
-    customDirectives[attribute];
+ddApp.attrOfEleExsistIn = function(isDefault,attribute){
+  var currentAttr = (isDefault)? ddApp.defaultDirectives[attribute]:
+    ddApp.customDirectives[attribute];
   if(currentAttr) {
     return true;
   }
   return false;
 }
-var levenshteinDistance = function(s, t) {
+ddApp.levenshteinDistance = function(s, t) {
     var d = [];
     var n = s.length;
     var m = t.length;
@@ -246,6 +248,6 @@ var levenshteinDistance = function(s, t) {
     return d[n][m];
 }
 
-checkDefaultDirs();
-getSuggestions();
-displayResults();
+ddApp.checkDefaultDirs();
+ddApp.getSuggestions();
+ddApp.displayResults();
