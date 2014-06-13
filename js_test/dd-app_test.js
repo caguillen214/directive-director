b3fdd0f41d7a391d03e44d3c06ec6520e97218ac
Carlo s A. Guillen <caguillen@google.com>
2014-06-12 18:18:11 -0700

    chore(dd-app): set up files to begin testing

---
+++ js_test/dd-app_test.js
@@ -0,0 +1,12 @@
+describe('dd-app', function() {
+  describe('windowElements', function() {
+    it('should be an HTMLCollection', function() {
+      console.log(windowElements)
+      expect(windowElements instanceof HTMLCollection).toBe(true);
+    })
+    it('should retrieve an array with the <html> element as the first spot', function() {
+      var all = windowElements;
+      expect(all[0].nodeName).toBe("HTML");
+    })
+  })
+})
