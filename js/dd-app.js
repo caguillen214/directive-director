8f57ee5b32aebca3e74b4fffa5ce5b6f1fb8b808
Carlo s A. Guillen <caguillen@google.com>
2014-06-13 10:18:43 -0700

    bug(dd-app): refactor dd-app.js to fix sending wrong types between functions. Correctly identify wrong attributes

--- js/dd-app.js
+++ js/dd-app.js
@@ -1,6 +1,83 @@
 var windowElements = document.getElementsByTagName("*");
 var DEFAULT = true, CUSTOM = false;
 var defaultDirectives = {
+  'abbr' : 'abbr',
+  'accept': 'accept',
+  'accesskey': 'accesskey',
+  'action': 'action',
+  'align': 'align',
+  'alt':'alt',
+  'background':'background',
+  'bgcolor': 'bgcolor',
+  'border': 'border',
+  'cellpadding': 'cellpadding',
+  'char': 'char',
+  'charoff': 'charoff',
+  'charset': 'charset',
+  'checked': 'checked',
+  'cite': 'cite',
+  'class': 'class',
+  'classid': 'classid',
+  'code': 'code',
+  'codebase': 'codebase',
+  'color': 'color',
+  'cols': 'cols',
+  'colspan': 'colspan',
+  'content': 'content',
+  'data': 'data',
+  'defer': 'defer',
+  'dir': 'dir',
+  'face': 'face',
+  'for': 'for',
+  'frame': 'frame',
+  'frameborder': 'frameborder',
+  'headers': 'headers',
+  'height': 'height',
+  'href': 'href',
+  'id': 'id',
+  'label': 'label',
+  'lang': 'lang',
+  'language': 'language',
+  'link': 'link',
+  'marginheight': 'marginheight',
+  'marginwidth': 'marginwidth',
+  'maxlength':'maxlength',
+  'media': 'media',
+  'multiple': 'multiple',
+  'name': 'name',
+  'object': 'object',
+  'onblur': 'onblur',
+  'onchange': 'onchange',
+  'onclick': 'onclick',
+  'onfocus': 'onfocus',
+  'onkeydown':'onkeydown',
+  'onkeypress': 'onkeypress',
+  'onkeyup': 'onkeyup',
+  'onload': 'onload',
+  'onmousedown': 'onmousedown',
+  'onmousemove': 'onmousemove',
+  'onmouseout': 'onmouseout',
+  'onmouseover': 'onmouseover',
+  'onmouseup': 'onmouseup',
+  'onreset': 'onreset',
+  'onselect': 'onselect',
+  'onsubmit': 'onsubmit',
+  'readonly': 'readonly',
+  'rel': 'rel',
+  'rev': 'rev',
+  'rows': 'rows',
+  'rowspan': 'rowspan',
+  'size': 'size',
+  'span': 'span',
+  'src': 'src',
+  'start': 'start',
+  'style': 'style',
+  'text': 'text',
+  'target': 'target',
+  'title': 'title',
+  'type': 'type',
+  'value': 'value',
+  'width': 'width',
   'ng-app': 'ng-app',
   'ng-bind': 'ng-bind',
   'ng-bindhtml': 'ng-bindhtml',
@@ -56,56 +133,78 @@ var defaultDirectives = {
 var customDirectives = {'ha-breadcrumbs':'ha-breadcrumbs'}
 var failedElements = [];
 var correctionsFound = [];
+
 var checkDefaultDirs = function() {
   for(var elesIndex = 0; elesIndex < windowElements.length; elesIndex++) {
     var attrsOfEle = windowElements[elesIndex].attributes;
     if(attrsOfEle.length){
-      var matchFound = attrsOfEleExsistIn(DEFAULT, attrsOfEle);
-      if(!matchFound && !inCustomDirs(attrsOfEle)) {
-        failedElements.push(windowElements[elesIndex]);
+      console.log(attrsOfEle);
+      failedAttributes = getFailedAttributes(attrsOfEle);
+      if(failedAttributes.length){
+        failedEle = {
+          htmlComponent:windowElements[elesIndex],
+          failedAttributes: failedAttributes
+        };
+        failedElements.push(failedEle);
       }
     }
   }
-  findClosestMatches();
 }
-var findClosestMatch = function() {
-  for(var failedIndex = 0; failedIndex < failedElements.length; failedIndex++) {
-    var min_levDist = Number.MAX_SAFE_INTEGER;
-    var closestMatch;
-    for(var customIndex = 0; customIndex < customDirectives.length; customIndex++) {
-      var dirFail = failedElements[failedIndex];
-      var dirCust = customDirectives[customIndex];
-      if(Math.abs(dirFail.length-dirCust.length)) {
-        var currentlevDist = levenshteinDistance(dirFail,dirCust);
-        min_levDist = (min_levDist > currentlevDist)? currentlevDist : min_levDist;
-        closestMatch = (min_levDist > currentlevDist)? dirFail : closestMatch;
+var getFailedAttributes = function(attributes) {
+  var failedAttributes = [];
+  for(var attrIndex = 0; attrIndex < attributes.length; attrIndex++) {
+    var attrName = attributes[attrIndex].nodeName;
+    var inDefault = attrOfEleExsistIn(DEFAULT, attrName);
+    var inCustom = attrOfEleExsistIn(CUSTOM, attrName);
+    if(!inDefault && !inCustom) {
+      var failedAttr = {
+        failedAttr: attrName,
+        correction: ''
       }
+      failedAttributes.push(failedAttr);
     }
-    for(var defaultIndex = 0; defaultIndex < defaultDirectives.length; defaultIndex++) {
-      var dirFail = failedElements[failedIndex];
-      var dirCust = defaultDirectives[defaultIndex];
-      if(Math.abs(dirFail.length-dirCust.length)) {
-        var currentlevDist = levenshteinDistance(dirFail,dirCust);
-        min_levDist = (min_levDist > currentlevDist)? currentlevDist : min_levDist;
-        closestMatch = (min_levDist > currentlevDist)? dirFail : closestMatch;
-      }
-    }
-    var toPush = {htmlComponent: failedElements[failedIndex], correction: closestMatch};
+  }
+  return failedAttributes;
+}
+var getSuggestions = function() {
+  for(var failedEleIndex = 0; failedEleIndex < failedElements.length; failedEleIndex++) {
+    var cDirMatch = findClosestMatchIn(CUSTOM, failedEleIndex);
+    var dDirMatch = findClosestMatchIn(DEFAULT, failedEleIndex);
+    var closestMatch = (Math.min(cDirMatch.levDist, dDirMatch.levDist)==cDirMatch.levDist)?
+      cDirMatch.match : dDirMatch.match;
+    var toPush = {htmlComponent: failedElements[failedEleIndex], correction: closestMatch};
     correctionsFound.push(toPush);
   }
 }
-
-var inCustomDirs = function(attrsOfEle) {
-  return attrsOfEleExsistIn(CUSTOM, attrsOfEle);
+var findClosestMatchIn = function(isDefault, currentEle) {
+  var min_levDist = Number.MAX_SAFE_INTEGER;
+  var toSearch = (isDefault)? defaultDirectives : customDirectives;
+  for(var index = 0; index < toSearch.length; index++) {
+    var failedEleAttrs = failedElements[currentEle].failedAttributes;
+    for(var failedAttrIndex = 0; failedAttrIndex < failedEleAttrs.length; failedAttrIndex++) {
+      var dirFail = failedEleAttrs[failedAttrIndex];
+      var dirCust = toSearch[index];
+      if(Math.abs(dirFail.length-dirCust.length) < 3) {
+        var currentlevDist = levenshteinDistance(dirFail,dirCust);
+        var min_levDist = (min_levDist > currentlevDist)? currentlevDist : min_levDist;
+        var closestMatch = (min_levDist > currentlevDist)? dirFail : closestMatch;
+      }
+    }
+  }
+  return {
+    match:closestMatch,
+    levDist:min_levDist
+  };
 }
-var attrsOfEleExsistIn = function(isDefault,attrsOfEle){
-  for(var attrIndex = 0; attrIndex < attrsOfEle.length; attrIndex++) {
-    var currentAttr = (isDefault)? defaultDirectives[attrsOfEle[attrIndex].nodeName]:
-      customDirectives[attrsOfEle[attrIndex].nodeName];
+var attrOfEleExsistIn = function(isDefault,attribute){
+  //for(var attrIndex = 0; attrIndex < attribute.length; attrIndex++) {
+    //attrName = attribute[attrIndex].nodeName;
+    var currentAttr = (isDefault)? defaultDirectives[attribute]:
+      customDirectives[attribute];
     if(currentAttr) {
       return true;
     }
-  }
+  //}
   return false;
 }
 var levenshteinDistance = function(s, t) {
@@ -139,7 +238,13 @@ var levenshteinDistance = function(s, t) {
     }
     return d[n][m];
 }
+
 checkDefaultDirs();
+//getSuggestions();
+console.log("windowElements")
 console.log(windowElements);
+console.log("failedElements")
 console.log(failedElements);
+// console.log("correctionsFound")
+// console.log(correctionsFound);
 
--- js_test/dd-app_test.js
+++ js_test/dd-app_test.js
@@ -1,11 +1,21 @@
 describe('dd-app', function() {
+  beforeEach(function() {
+    workspace = document.createElement('div');
+    //workspace.setAttribute('id','workspace');
+    workspace.appendChild('div');
+    workspace.setAttribute('id')
+  })
   describe('windowElements', function() {
     it('should be an HTMLCollection', function() {
       expect(windowElements instanceof HTMLCollection).toBe(true);
     })
-    it('should retrieve an array with the <html> element as the first spot', function() {
+    it('should be an array with the <html> element as the first element', function() {
       var all = windowElements;
       expect(all[0].nodeName).toBe("HTML");
     })
+  })
+
+  describe('should fill windowElements with exsisting HTML DOM elements',function() {
+
   })
 })
